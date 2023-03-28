6e1c2695f59f228d2140dc2203113ada7f151edf
najamoe <naja0718@stud.kea.dk>
2023-03-28 10:52:58 +0200

    Created model classes

--- src/main/java/com/example/demo11/Model/Items.java
+++ src/main/java/com/example/demo11/Model/Items.java
@@ -1,4 +1,9 @@
 package com.example.demo11.Model;
 
 public class Items {
+    private String name;
+    private int quantity;
+    private String description;
+    private double price;
+    private String URL;
 }
---
+++ src/main/java/com/example/demo11/Model/WishLists.java
@@ -0,0 +1,8 @@
+package com.example.demo11.Model;
+
+public class WishLists {
+    private String name;
+    private String URL;
+
+
+}
