9017e64038ca98becc2001ed092bb99ee3a13f2b
najamoe <najamoe@outlook.dk>
2023-03-29 12:30:11 +0200

    started signupform

--- src/main/java/com/example/MiniProject/Controller/WishController.java
+++ src/main/java/com/example/MiniProject/Controller/WishController.java
@@ -2,19 +2,26 @@ package com.example.MiniProject.Controller;
 
 import Service.ServiceWish;
 import com.example.MiniProject.Model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/wishFreaks")
 public class WishController {
 
     private ServiceWish serviceWish;
 
+    public WishController(ServiceWish serviceWish) {
+        this.serviceWish = serviceWish;
+    }
+
+    @GetMapping("/signup")
+    public String signupform(Model model){
+        model.addAttribute("user", "userSignup");
+        return "signUp";
+    }
     @PostMapping("/signup")
     public ResponseEntity<String> createUser(@RequestBody User user) {
         try {
