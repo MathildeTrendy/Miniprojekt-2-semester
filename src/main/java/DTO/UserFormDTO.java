3557040c62756dc51ac050ff17d25f5f86cbc094
MathildeTrendy <113180048+MathildeTrendy@users.noreply.github.com>
2023-04-12 10:49:25 +0200

    DTO done

---
+++ src/main/java/DTO/UserFormDTO.java
@@ -0,0 +1,58 @@
+package DTO;
+
+public class UserFormDTO {
+
+    private long user_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserFormDTO(long user_id, String firstName, String lastName, String email, String password) {
+        this.user_id = user_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+    public UserFormDTO(){}
+
+    public long getUser_id() {
+        return user_id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
